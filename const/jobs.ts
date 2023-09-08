5054816876b00a8fa5be2c62885c9ec442d22e0f
weisintai <wstai1214@gmail.com>
2023-09-08 15:00:42 +0800

    Add hardcoded job posts

---
+++ const/jobs.ts
@@ -0,0 +1,739 @@
+export const data = {
+  job_listings: [
+    {
+      id: 168289,
+      job_path:
+        "/jobs/hcltech-applications-solution-architect-public-sector-168289?utm_campaign=job_search\u0026utm_medium=listing\u0026utm_source=nodeflair_jobs",
+      position: "Solutions",
+      title: "Applications Solution Architect (Public Sector)",
+      salary_min: 12091,
+      salary_max: 16706,
+      currency: null,
+      remuneration_frequency: "Monthly",
+      tech_stacks: [
+        {
+          name: "AWS",
+        },
+        {
+          name: "Azure",
+        },
+      ],
+      seniority: ["Senior"],
+      time_ago: "13 minutes",
+      company: {
+        id: 1035,
+        rating: 3.6,
+        companyname: "HCLTech",
+        avatar: "https://nodeflair.com/companies/1035.png",
+      },
+      is_salary_estimated: true,
+      formatted_salary_min: "S$12,091",
+      formatted_salary_max: "S$16,706",
+      country: "Singapore",
+    },
+    {
+      id: 168288,
+      job_path:
+        "/jobs/manulife-senior-c-engineer-168288?utm_campaign=job_search\u0026utm_medium=listing\u0026utm_source=nodeflair_jobs",
+      position: "Fullstack",
+      title: "Senior C++ Engineer",
+      salary_min: null,
+      salary_max: null,
+      currency: null,
+      remuneration_frequency: "Monthly",
+      tech_stacks: [
+        {
+          name: "New Relic",
+        },
+        {
+          name: "C++",
+        },
+        {
+          name: "Jenkins",
+        },
+        {
+          name: "Dynatrace",
+        },
+        {
+          name: "ELK",
+        },
+        {
+          name: "Fortify",
+        },
+        {
+          name: "SonarQube",
+        },
+        {
+          name: "kafka",
+        },
+        {
+          name: "Redux",
+        },
+        {
+          name: "JavaScript",
+        },
+        {
+          name: "SQL",
+        },
+        {
+          name: "Node.js",
+        },
+        {
+          name: "GraphQL",
+        },
+        {
+          name: "React",
+        },
+      ],
+      seniority: ["Senior"],
+      time_ago: "13 minutes",
+      company: {
+        id: 2373,
+        rating: 4.0,
+        companyname: "Manulife ",
+        avatar: "https://nodeflair.com/companies/2373.png",
+      },
+      is_salary_estimated: true,
+      formatted_salary_min: "",
+      formatted_salary_max: "",
+      country: "Philippines",
+    },
+    {
+      id: 168287,
+      job_path:
+        "/jobs/etrading-software-software-developer-java-fullstack-168287?utm_campaign=job_search\u0026utm_medium=listing\u0026utm_source=nodeflair_jobs",
+      position: "Fullstack",
+      title: "Software Developer (Java Fullstack)",
+      salary_min: null,
+      salary_max: null,
+      currency: null,
+      remuneration_frequency: "Monthly",
+      tech_stacks: [
+        {
+          name: "AWS",
+        },
+        {
+          name: "Spring",
+        },
+        {
+          name: "CI",
+        },
+        {
+          name: "Java",
+        },
+        {
+          name: "Linux",
+        },
+        {
+          name: "Angular",
+        },
+        {
+          name: "JavaScript",
+        },
+        {
+          name: "jQuery",
+        },
+        {
+          name: "React",
+        },
+      ],
+      seniority: ["Mid"],
+      time_ago: "15 minutes",
+      company: {
+        id: 12561,
+        rating: 4.6,
+        companyname: "Etrading Software",
+        avatar: "https://nodeflair.com/companies/12561.png",
+      },
+      is_salary_estimated: true,
+      formatted_salary_min: "",
+      formatted_salary_max: "",
+      country: "Philippines",
+    },
+    {
+      id: 168286,
+      job_path:
+        "/jobs/gcash-automation-solutions-manager-168286?utm_campaign=job_search\u0026utm_medium=listing\u0026utm_source=nodeflair_jobs",
+      position: "Others",
+      title: "Automation Solutions Manager",
+      salary_min: null,
+      salary_max: null,
+      currency: null,
+      remuneration_frequency: "Monthly",
+      tech_stacks: [
+        {
+          name: "Rest API",
+        },
+        {
+          name: "Microsoft",
+        },
+        {
+          name: "Java",
+        },
+        {
+          name: "Python",
+        },
+      ],
+      seniority: ["Manager"],
+      time_ago: "15 minutes",
+      company: {
+        id: 12568,
+        rating: 3.6,
+        companyname: "GCash",
+        avatar: "https://nodeflair.com/companies/12568.png",
+      },
+      is_salary_estimated: true,
+      formatted_salary_min: "",
+      formatted_salary_max: "",
+      country: "Philippines",
+    },
+    {
+      id: 168285,
+      job_path:
+        "/jobs/cambridge-university-press-assessment-senior-software-engineer-3835-168285?utm_campaign=job_search\u0026utm_medium=listing\u0026utm_source=nodeflair_jobs",
+      position: "Fullstack",
+      title: "Senior Software Engineer - 3835",
+      salary_min: null,
+      salary_max: null,
+      currency: null,
+      remuneration_frequency: "Monthly",
+      tech_stacks: [
+        {
+          name: "AWS",
+        },
+        {
+          name: "bower",
+        },
+        {
+          name: "Grunt",
+        },
+        {
+          name: "Git",
+        },
+        {
+          name: "HTML",
+        },
+        {
+          name: "npm",
+        },
+        {
+          name: "Typescript",
+        },
+        {
+          name: "JavaScript",
+        },
+        {
+          name: "Node.js",
+        },
+        {
+          name: "GraphQL",
+        },
+        {
+          name: "CSS",
+        },
+        {
+          name: "Vue.js",
+        },
+      ],
+      seniority: ["Senior"],
+      time_ago: "17 minutes",
+      company: {
+        id: 18359,
+        rating: 0.0,
+        companyname: "Cambridge University Press \u0026 Assessment",
+        avatar: "https://nodeflair.com/companies/18359.png",
+      },
+      is_salary_estimated: true,
+      formatted_salary_min: "",
+      formatted_salary_max: "",
+      country: "Philippines",
+    },
+    {
+      id: 168284,
+      job_path:
+        "/jobs/cloudpay-senior-software-engineer-168284?utm_campaign=job_search\u0026utm_medium=listing\u0026utm_source=nodeflair_jobs",
+      position: "Fullstack",
+      title: "Senior Software Engineer",
+      salary_min: null,
+      salary_max: null,
+      currency: null,
+      remuneration_frequency: "Monthly",
+      tech_stacks: [
+        {
+          name: "MariaDB",
+        },
+        {
+          name: "DynamoDB",
+        },
+        {
+          name: "Rest API",
+        },
+        {
+          name: "Docker",
+        },
+        {
+          name: "AngularJS",
+        },
+        {
+          name: "Spring",
+        },
+        {
+          name: "JPA",
+        },
+        {
+          name: "OOP",
+        },
+        {
+          name: "Groovy",
+        },
+        {
+          name: "Java",
+        },
+        {
+          name: "NoSQL",
+        },
+        {
+          name: "Kubernetes",
+        },
+        {
+          name: "Springboot",
+        },
+        {
+          name: "JavaScript",
+        },
+        {
+          name: "jQuery",
+        },
+        {
+          name: "Node.js",
+        },
+        {
+          name: "MySQL",
+        },
+        {
+          name: "MongoDB",
+        },
+        {
+          name: "Vue.js",
+        },
+      ],
+      seniority: ["Senior"],
+      time_ago: "18 minutes",
+      company: {
+        id: 2161,
+        rating: 3.4,
+        companyname: "CloudPay",
+        avatar: "https://nodeflair.com/companies/2161.png",
+      },
+      is_salary_estimated: true,
+      formatted_salary_min: "",
+      formatted_salary_max: "",
+      country: "Philippines",
+    },
+    {
+      id: 168283,
+      job_path:
+        "/jobs/quantrics-enterprises-python-developer-expert-work-from-home-168283?utm_campaign=job_search\u0026utm_medium=listing\u0026utm_source=nodeflair_jobs",
+      position: "Fullstack",
+      title: "Python Developer Expert (Work From Home)",
+      salary_min: 86667,
+      salary_max: 95000,
+      currency: null,
+      remuneration_frequency: "Monthly",
+      tech_stacks: [
+        {
+          name: "Python",
+        },
+      ],
+      seniority: ["Senior"],
+      time_ago: "19 minutes",
+      company: {
+        id: 7223,
+        rating: 0.0,
+        companyname: "Quantrics Enterprises",
+        avatar: "https://nodeflair.com/companies/7223.png",
+      },
+      is_salary_estimated: true,
+      formatted_salary_min: "₱86,667",
+      formatted_salary_max: "₱95,000",
+      country: "Philippines",
+    },
+    {
+      id: 168282,
+      job_path:
+        "/jobs/ncr-corporation-software-sr-developer-168282?utm_campaign=job_search\u0026utm_medium=listing\u0026utm_source=nodeflair_jobs",
+      position: "Fullstack",
+      title: "Software Sr. Developer",
+      salary_min: null,
+      salary_max: null,
+      currency: null,
+      remuneration_frequency: "Monthly",
+      tech_stacks: [
+        {
+          name: "TCP",
+        },
+        {
+          name: "SVN",
+        },
+        {
+          name: "C++",
+        },
+        {
+          name: "Jenkins",
+        },
+        {
+          name: "Visual Studio",
+        },
+        {
+          name: "Java",
+        },
+        {
+          name: "Asp.net",
+        },
+        {
+          name: "JavaScript",
+        },
+        {
+          name: "C#",
+        },
+        {
+          name: "jQuery",
+        },
+      ],
+      seniority: ["Senior"],
+      time_ago: "20 minutes",
+      company: {
+        id: 506,
+        rating: 3.5,
+        companyname: "NCR Corporation",
+        avatar: "https://nodeflair.com/companies/506.png",
+      },
+      is_salary_estimated: true,
+      formatted_salary_min: "",
+      formatted_salary_max: "",
+      country: "Philippines",
+    },
+    {
+      id: 168281,
+      job_path:
+        "/jobs/hogarth-senior-front-end-engineer-168281?utm_campaign=job_search\u0026utm_medium=listing\u0026utm_source=nodeflair_jobs",
+      position: "Frontend",
+      title: "Senior Front-End Engineer",
+      salary_min: null,
+      salary_max: null,
+      currency: null,
+      remuneration_frequency: "Monthly",
+      tech_stacks: [
+        {
+          name: "TDD",
+        },
+        {
+          name: "C++",
+        },
+        {
+          name: "AngularJS",
+        },
+        {
+          name: "WebRTC",
+        },
+        {
+          name: "CI",
+        },
+        {
+          name: "Flow",
+        },
+        {
+          name: "Less",
+        },
+        {
+          name: "Webpack",
+        },
+        {
+          name: "OOP",
+        },
+        {
+          name: "Grunt",
+        },
+        {
+          name: "Git",
+        },
+        {
+          name: "SASS",
+        },
+        {
+          name: "WebGL",
+        },
+        {
+          name: "Azure",
+        },
+        {
+          name: "HTML",
+        },
+        {
+          name: "Angular",
+        },
+        {
+          name: "JavaScript",
+        },
+        {
+          name: "C#",
+        },
+        {
+          name: "Node.js",
+        },
+        {
+          name: "React",
+        },
+        {
+          name: "CSS",
+        },
+        {
+          name: "Vue.js",
+        },
+      ],
+      seniority: ["Senior"],
+      time_ago: "21 minutes",
+      company: {
+        id: 2431,
+        rating: 3.4,
+        companyname: "Hogarth",
+        avatar: "https://nodeflair.com/companies/2431.png",
+      },
+      is_salary_estimated: true,
+      formatted_salary_min: "",
+      formatted_salary_max: "",
+      country: "Philippines",
+    },
+    {
+      id: 168280,
+      job_path:
+        "/jobs/asurion-data-engineer-3-ph-168280?utm_campaign=job_search\u0026utm_medium=listing\u0026utm_source=nodeflair_jobs",
+      position: "Data Eng",
+      title: "Data Engineer 3 (PH)",
+      salary_min: null,
+      salary_max: null,
+      currency: null,
+      remuneration_frequency: "Monthly",
+      tech_stacks: [
+        {
+          name: "ETL",
+        },
+        {
+          name: "Informatica",
+        },
+        {
+          name: "RDS",
+        },
+        {
+          name: "DynamoDB",
+        },
+        {
+          name: "AWS",
+        },
+        {
+          name: "C++",
+        },
+        {
+          name: "Jenkins",
+        },
+        {
+          name: "Go",
+        },
+        {
+          name: "Analytics",
+        },
+        {
+          name: "CI",
+        },
+        {
+          name: "SSIS",
+        },
+        {
+          name: "Talend",
+        },
+        {
+          name: "Aurora",
+        },
+        {
+          name: "EMR",
+        },
+        {
+          name: "EC2",
+        },
+        {
+          name: "RedShift",
+        },
+        {
+          name: "Bamboo",
+        },
+        {
+          name: "Presto",
+        },
+        {
+          name: "Azure",
+        },
+        {
+          name: "Java",
+        },
+        {
+          name: "Spark",
+        },
+        {
+          name: "NoSQL",
+        },
+        {
+          name: "kafka",
+        },
+        {
+          name: "SQL",
+        },
+        {
+          name: "Scala",
+        },
+        {
+          name: "Hadoop",
+        },
+        {
+          name: "Python",
+        },
+      ],
+      seniority: ["Mid"],
+      time_ago: "22 minutes",
+      company: {
+        id: 3249,
+        rating: 3.6,
+        companyname: "Asurion",
+        avatar: "https://nodeflair.com/companies/3249.png",
+      },
+      is_salary_estimated: true,
+      formatted_salary_min: "",
+      formatted_salary_max: "",
+      country: "Philippines",
+    },
+    {
+      id: 168279,
+      job_path:
+        "/jobs/fujitsu-sr-application-systems-engineer-consultant-168279?utm_campaign=job_search\u0026utm_medium=listing\u0026utm_source=nodeflair_jobs",
+      position: "Syst. Eng",
+      title: "Sr. Application Systems Engineer/Consultant",
+      salary_min: null,
+      salary_max: null,
+      currency: null,
+      remuneration_frequency: "Monthly",
+      tech_stacks: [
+        {
+          name: "VB.Net",
+        },
+        {
+          name: "Spring",
+        },
+        {
+          name: "Java",
+        },
+        {
+          name: "iOS",
+        },
+        {
+          name: "Android",
+        },
+        {
+          name: "C#",
+        },
+        {
+          name: ".NET",
+        },
+      ],
+      seniority: ["Senior"],
+      time_ago: "23 minutes",
+      company: {
+        id: 1055,
+        rating: 3.5,
+        companyname: "Fujitsu",
+        avatar: "https://nodeflair.com/companies/1055.png",
+      },
+      is_salary_estimated: true,
+      formatted_salary_min: "",
+      formatted_salary_max: "",
+      country: "Philippines",
+    },
+    {
+      id: 168278,
+      job_path:
+        "/jobs/hogarth-senior-full-stack-software-engineer-168278?utm_campaign=job_search\u0026utm_medium=listing\u0026utm_source=nodeflair_jobs",
+      position: "Fullstack",
+      title: "Senior Full Stack Software Engineer",
+      salary_min: null,
+      salary_max: null,
+      currency: null,
+      remuneration_frequency: "Monthly",
+      tech_stacks: [
+        {
+          name: "TDD",
+        },
+        {
+          name: "AWS",
+        },
+        {
+          name: "YAML",
+        },
+        {
+          name: "CI",
+        },
+        {
+          name: "Microsoft",
+        },
+        {
+          name: ".Net Core",
+        },
+        {
+          name: "Git",
+        },
+        {
+          name: "Azure",
+        },
+        {
+          name: "npm",
+        },
+        {
+          name: "Angular",
+        },
+        {
+          name: "Typescript",
+        },
+        {
+          name: "JavaScript",
+        },
+        {
+          name: "C#",
+        },
+        {
+          name: "Node.js",
+        },
+        {
+          name: ".NET",
+        },
+        {
+          name: "React",
+        },
+        {
+          name: "Python",
+        },
+      ],
+      seniority: ["Senior"],
+      time_ago: "23 minutes",
+      company: {
+        id: 2431,
+        rating: 3.4,
+        companyname: "Hogarth",
+        avatar: "https://nodeflair.com/companies/2431.png",
+      },
+      is_salary_estimated: true,
+      formatted_salary_min: "",
+      formatted_salary_max: "",
+      country: "Philippines",
+    },
+  ],
+  total_listings_count: 8013,
+  filter_recommendations: null,
+  has_job_alert: false,
+};
